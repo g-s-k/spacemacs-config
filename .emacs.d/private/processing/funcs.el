732082b4d8329df8904e6ed4702393d9bf4ef9e8
George Kaplan <george@georgekaplan.xyz>
2019-11-26 00:31:57 -0500

    use existing code to compile

--- .emacs.d/private/processing/funcs.el
+++
@@ -1,6 +0,0 @@
-(defun spacemacs/run-processing-sketch ()
-  "Run Processing sketch"
-  (interactive)
-  (compile
-   (format "processing-java --sketch=%s --run"
-           (shell-quote-argument default-directory))))
--- .emacs.d/private/processing/packages.el
+++ .emacs.d/private/processing/packages.el
@@ -8,7 +8,16 @@
   (use-package processing-mode
     :defer t
     :mode (("\\.pde\\'" . processing-mode))
-    :config
+    :init
     (progn
-      (spacemacs/set-leader-keys-for-major-mode 'processing-mode
-        "r" 'spacemacs/run-processing-sketch))))
+      (setenv "PATH" (concat (getenv "PATH") ":/bin"))
+      (setq-default processing-location
+                    (concat
+                     (file-name-as-directory (exec-path-from-shell-getenv "HOME"))
+                     (file-name-as-directory "processing-3.5.3")
+                     "processing-java")))
+    :config
+    (spacemacs/set-leader-keys-for-major-mode 'processing-mode
+      "b" 'processing-sketch-build
+      "p" 'processing-sketch-present
+      "r" 'processing-sketch-run)))
